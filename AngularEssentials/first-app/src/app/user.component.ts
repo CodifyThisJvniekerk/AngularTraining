499ffa23af5482eb4827ffd458b33947adc56e8d
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-03-02 07:57:55 +0200

    Binding Component Properties

--- AngularEssentials/first-app/src/app/user.component.ts
+++ AngularEssentials/first-app/src/app/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -9,5 +9,5 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
-  name = 'Johan'
+  @Input() name = 'Johan'
 }
