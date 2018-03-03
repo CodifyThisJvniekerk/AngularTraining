f5b6b2d71971c5ea9a53145a961537468d4d5ade
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-03-03 09:56:55 +0200

    ng g component componentname

--- AngularEssentials/first-app/src/app/user.component.ts
+++ AngularEssentials/first-app/src/app/user.component.ts
@@ -5,7 +5,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   template: `
   <input type="text" (input)="onUserInput($event)" />
   <p> User Component Here</p>
-  <p> hello {{ name }} </p>`
+  <p> hello {{ name }} </p>
+  <app-user-detail></app-user-detail>`
 })
 
 export class UserComponent {
