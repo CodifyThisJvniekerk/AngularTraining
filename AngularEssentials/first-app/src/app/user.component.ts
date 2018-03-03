896cf8e0c65122f54a520678a7a5d0e5bbd0764c
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-03-03 09:44:16 +0200

    Listening to your Custom Events

--- AngularEssentials/first-app/src/app/user.component.ts
+++ AngularEssentials/first-app/src/app/user.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
-  <input type="text" [(ngModel)]='name' />
+  <input type="text" (input)="onUserInput($event)" />
   <p> User Component Here</p>
   <p> hello {{ name }} </p>`
 })
 
 export class UserComponent {
-  name = 'Johan'
+  @Input() name;
+  @Output() nameChanged = new EventEmitter<string>();
+
+  onUserInput(event) {
+    // this.name = event.target.value
+    this.nameChanged.emit(event.target.value);
+  }
 }
