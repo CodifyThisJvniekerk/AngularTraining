d3e91205379463148c6728b1d21eea69fb56abfa
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-03-09 09:47:38 +0200

    Angular Forms with validation

--- AngularEssentials/WorkTraining/angular-forms/src/app/hero-form/hero-form.component.ts
+++ AngularEssentials/WorkTraining/angular-forms/src/app/hero-form/hero-form.component.ts
@@ -10,7 +10,7 @@ export class HeroFormComponent implements OnInit {
   powers = ['Realy Smart', 'Super Flexible',
     'Super Hot', 'Weather Changer'];
 
-  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+  public model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
 
   submitted = false;
   constructor() { }
@@ -24,6 +24,10 @@ export class HeroFormComponent implements OnInit {
   // TODO: Remove this when we're done
   get diagnostic() {
     return JSON.stringify(this.model);
+  }
+
+  newHero() {
+    this.model = new Hero(42, '', '', '');
   }
 
 }
