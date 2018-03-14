59d612580de423bce06b3c6c079023baa06e3845
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-03-14 20:10:30 +0200

    Managing Data in our Application

--- AngularEssentials/prj-cmp-directives-start/src/app/tabs/tabs.component.ts
+++ AngularEssentials/prj-cmp-directives-start/src/app/tabs/tabs.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    {name: 'Luke Skywalker', side: ''},
+    {name: 'Darth Vader', side: ''},
+    {name: 'Princess Lia', side: ''},
+    {name: 'R2D2', side: ''}
+  ]
+
+  chosenList = 'all';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  choseList(side: string) {
+    this.chosenList = side;
+  }
+
+  getCharacters(): any {
+    if (this.chosenList === 'all') {
+      return this.characters.slice();
+    }
+    return this.characters.filter((char) => {
+      return char.side === this.chosenList}
+    );
+  }
 }
