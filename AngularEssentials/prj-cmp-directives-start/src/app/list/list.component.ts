15729116e83778cd8517e173bbdc62ef3a80eb97
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-04-09 19:56:24 +0200

    Child Routes
    Configure and use child routes

--- AngularEssentials/prj-cmp-directives-start/src/app/list/list.component.ts
+++ AngularEssentials/prj-cmp-directives-start/src/app/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, EventEmitter } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { StarWarsService } from '../star-wars-service';
 
 @Component({
   selector: 'app-list',
@@ -6,11 +8,21 @@ import { Component, OnInit, Input, EventEmitter } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  @Input() characters;
+  characters = [];
+  activatedRoute: ActivatedRoute;
+  swService: StarWarsService
 
-  constructor() { }
+  constructor(activeRoute: ActivatedRoute, swservice: StarWarsService) {
+    this.activatedRoute = activeRoute;
+    this.swService = swservice;
+   }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params) => {
+        this.characters = this.swService.getCharacters(params.side);
+      }
+    );
   }
 
 }
