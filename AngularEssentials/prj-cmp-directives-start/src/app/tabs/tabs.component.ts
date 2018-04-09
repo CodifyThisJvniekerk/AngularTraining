6faf65bd93b82fe04d67952448db6f160d9a823a
CodifyThisJvniekerk <jvniekerk.it@gmail.com>
2018-04-09 20:18:11 +0200

    Clean up dead code

--- AngularEssentials/prj-cmp-directives-start/src/app/tabs/tabs.component.ts
+++ AngularEssentials/prj-cmp-directives-start/src/app/tabs/tabs.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { StarWarsService } from '../star-wars-service';
 
 @Component({
   selector: 'app-tabs',
@@ -7,26 +6,8 @@ import { StarWarsService } from '../star-wars-service';
   styleUrls: ['./tabs.component.css']
 })
 export class TabsComponent implements OnInit {
-  characters = [];
 
-  chosenList = 'all';
+  constructor() {}
 
-  swService: StarWarsService;
-
-  constructor(swService: StarWarsService) {
-    this.swService = swService;
-   }
-
-  ngOnInit() {
-    this.getCharacters();
-  }
-
-  onChoose(side: string) {
-    this.chosenList = side;
-    this.getCharacters();
-  }
-
-  getCharacters(): any {
-   this.characters = this.swService.getCharacters(this.chosenList)
-  }
+  ngOnInit() {}
 }
